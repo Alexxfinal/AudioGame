a4f6a28ff097e074c5e4f73e03323b13e5e80029
Alexxfinal <alexxfinal@hotmail.com>
2018-02-16 12:46:20 -0800

    Initial commit

---
+++ AudioCheck/Assets/FollowMouse.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+
+	void Update ()
+    {
+        //transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, transform.position.y, transform.position.z);
+
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+
+        transform.position = position;
+    }
+}
---
+++ AudioCheck/Assets/Test.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioListener))]
+public class Test : MonoBehaviour
+{
+    public GameObject circle;
+    int k = 16; //16 checks
+    float position = -8;
+    public float maxValueToCheck = 1;
+
+    public float[] spectrum = new float[256];
+
+    void Update()
+    {
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        for (int i = 0; i < k ; i++)
+        {
+            spawnCircleIfNeeded(spectrum, k * i, k + k*i);
+            position += 1;
+
+            if (position > 8f) position = -8f;
+        }
+    }
+
+    void spawnCircleIfNeeded(float[] spectrum, int startIndex, int endIndex)
+    {
+        float totalSum = 0;
+
+        for (; startIndex < endIndex; startIndex++)
+        {
+            totalSum += spectrum[startIndex];
+        }
+
+        if (totalSum > maxValueToCheck)
+        {
+            GameObject myCircle;
+            myCircle = Instantiate(circle);
+
+            myCircle.transform.position = new Vector3(position, myCircle.transform.position.y, myCircle.transform.position.z);
+        }
+            
+
+    }
+}
